83f278c8798853b3911054bad544fd826759c3e0
Rubi Thapa <habi.thapa@gmail.com>
2024-02-24 20:46:27 +0545

    View Employee list completed

--- src/pages/Employee/ViewEmployee/index.jsx
+++ src/pages/Employee/ViewEmployee/index.jsx
@@ -1,12 +1,21 @@
 import { Box } from "@mui/material";
-import axiosInstance from "../../../api";
-import ENDPOINTS from "../../../api/ENDPOINTS";
 import { useEffect, useState } from "react";
 import fetchEmployees from "../services/fetchEmployee";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 150 },
+  { field: "first_name", headerName: "First Name", width: 150 },
+  { field: "middle_name", headerName: "Middle Name", width: 150 },
+  { field: "last_name", headerName: "Last Name", width: 150 },
+  { field: "gender", headerName: "Gender", width: 150 },
+  { field: "age", headerName: "Age", width: 150 },
+  { field: "address", headerName: "Address", width: 150 },
+  { field: "contact_details", headerName: "Contact Detail", width: 150 },
+];
 
 const ViewEmployee = () => {
   const [employeesData, setEmployeesData] = useState([]);
-
   useEffect(() => {
     fetchEmployees()
       .then((response) => {
@@ -16,6 +25,7 @@ const ViewEmployee = () => {
         console.error("Error:", error);
       });
   }, []);
+  const minHeight = employeesData.length > 0 ? "auto" : 500; // Set a minimum height when there is no data
 
   return (
     <Box
@@ -23,9 +33,10 @@ const ViewEmployee = () => {
         marginTop: 8,
         display: "flex",
         flexDirection: "column",
+        height: minHeight,
       }}
     >
-      Hello
+      <DataGrid rows={employeesData} columns={columns} />
     </Box>
   );
 };
